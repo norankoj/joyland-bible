ca844070c96d71a3465043621191b7b1442e990c
노나연 <norankoj@naver.com>
2026-08-13 14:02:43 +0900

    fix: superbase 설정 추가

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
-// ✅ Supabase 설정값을 여기에 입력하세요
 // Supabase 대시보드 > Project Settings > API 에서 복사
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = "https://qdsajqulyqnsitdcevul.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkc2FqcXVseXFuc2l0ZGNldnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2ODIzMDUsImV4cCI6MjA4ODI1ODMwNX0.BMZQLcSjXa1tq7QmnMAa8GdaAQ98nPsf0L6ZtrJSeG4";
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
